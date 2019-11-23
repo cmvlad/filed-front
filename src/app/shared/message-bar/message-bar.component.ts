ce7cdd4bf566998ff1bc5a33ebfc551892b94d94
cmvlad <Me27478328>
2019-11-23 20:37:11 +0000

    unsubscribed to message bar observable

--- src/app/shared/message-bar/message-bar.component.ts
+++ src/app/shared/message-bar/message-bar.component.ts
@@ -1,22 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MessageModel} from './models/message.model';
-import {MessageService} from 'primeng/api';
 import {MessageBarService} from './message-bar.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-message-bar',
   templateUrl: './message-bar.component.html',
   styleUrls: ['./message-bar.component.scss']
 })
-export class MessageBarComponent implements OnInit {
+export class MessageBarComponent implements OnInit, OnDestroy {
 
   private messageObject: MessageModel;
+  private messageBarServiceSubscription: Subscription;
 
   constructor(private messageBarService: MessageBarService) {
   }
 
   ngOnInit() {
-    this.messageBarService.messageBar$.subscribe(async message => {
+    this.messageBarServiceSubscription = this.messageBarService.messageBar$.subscribe(async message => {
       this.messageObject = message;
       await this.delay(2000);
       this.messageObject = new MessageModel();
@@ -25,6 +26,10 @@ export class MessageBarComponent implements OnInit {
 
   delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  ngOnDestroy(): void {
+    this.messageBarServiceSubscription.unsubscribe();
   }
 
 }
